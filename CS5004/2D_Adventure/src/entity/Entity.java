064f852b18d078688f35c26bd9cf1f7893bf5011
Tony <robert.a.j.wilson1986@gmail.com>
2022-03-10 22:27:55 -0500

    Background, Sprite, Tile, Movement, Game Loop
    background
    -load from txt file
    -use swing to create a window
    -set default sizes
    Sprite
    -load png
    -animate player
    Tile
    -load png
    Movement
    -animate character
    Game Loop
    -create game loop

---
+++ CS5004/2D_Adventure/src/entity/Entity.java
@@ -0,0 +1,21 @@
+package entity;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * This stores variables that will be used in player, monster, and NPC classes
+ * @author Robert Wilson
+ *
+ */
+public abstract class Entity {
+	
+	public int x, y;
+	public int speed;
+	
+	// Describes an image with an accessible buffer of image data.
+	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+	public String direction;
+	
+	public int spriteCounter = 0;
+	public int spriteNum = 1;
+}
---
+++ CS5004/2D_Adventure/src/main/KeyHandler.java
@@ -0,0 +1,47 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+	public boolean upPressed, downPressed, ltPressed, rtPressed;
+	
+	@Override
+	public void keyTyped(KeyEvent e) {		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		int code = e.getKeyCode();
+		
+		if(code == KeyEvent.VK_W) {
+			upPressed = true;
+		} else if(code == KeyEvent.VK_S) {
+			downPressed = true;
+		} else if(code == KeyEvent.VK_A) {
+			ltPressed = true;
+		} else if(code == KeyEvent.VK_D) {
+			rtPressed = true;
+		} 
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+		int code = e.getKeyCode();
+		
+		if(code == KeyEvent.VK_W) {
+			upPressed = false;
+		} else if(code == KeyEvent.VK_S) {
+			downPressed = false;
+		} else if(code == KeyEvent.VK_A) {
+			ltPressed = false;
+		} else if(code == KeyEvent.VK_D) {
+			rtPressed = false;
+		} 
+		
+	}
+
+}
