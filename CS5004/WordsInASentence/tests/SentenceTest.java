67a9f28690af609715078935659215ffb1d2f90d
Tony <robert.a.j.wilson1986@gmail.com>
2022-03-30 00:01:13 -0400

    Refactor and testing
    JUnit
    -100%
    Abstracted SentenceNodes
    -created abstract class SentenceNode
    Refactored
    -removed unnecessary fluff

--- CS5004/WordsInASentence/tests/SentenceTest.java
+++ CS5004/WordsInASentence/tests/SentenceTest.java
@@ -13,6 +13,10 @@ public class SentenceTest {
 	private SentenceList s4;
 	private SentenceList s5;
 	private SentenceList s6;
+	private SentenceList s7;
+	private SentenceList s8;
+	private SentenceList s9;
+	private SentenceList s10;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -22,10 +26,30 @@ public class SentenceTest {
 		s4 = new SentenceList(s2);
 		s5 = s2.clone();
 		s6 = s5.clone();
+		s7 = s1.clone();
+		s8 = s3.clone();
+		s9 = s3.clone();
+		s10 = new SentenceList("Why this is atrotious!", s3);
 	}
 
 	/**
-	 * tests get method
+	 * tests that every constructor works
+	 */
+	@Test
+	public void toStringTest() {
+		assertEquals("",s1.toString());
+		assertEquals("Hello ",s2.toString());
+		assertEquals("Hello World! My name is Tony.",s3.toString());
+		assertEquals("Hello ",s4.toString());
+		assertEquals("Hello ",s5.toString());
+		assertEquals("Hello ",s6.toString());
+		assertEquals("",s7.toString());
+		assertEquals("Hello World! My name is Tony.",s8.toString());
+		assertEquals("Why this is atrotious!Hello World! My name is Tony.",s10.toString());
+	}
+	
+	/**
+	 * tests get methods
 	 */
 	@Test
 	public void getTest() {
@@ -79,11 +103,17 @@ public class SentenceTest {
 		s5.add(0, "");
 		s5.add(1, ".");
 		s6.add(1, " ");
+		s7.add(0, "word", true);
+		s8.add(s8.getSize()-1, "Why hello Mark!");
+		s9.add(1, "Why hello Mark!");
 		assertEquals("Good.bye.bye. world.", s1.toString());
 		assertEquals("HelloWorld ", s2.toString());
 		assertEquals("Hello World! My name is Tony. It isn't Mark!", s3.toString());
 		assertEquals("Hello. ",s5.toString());
 		assertEquals("Hello  ", s6.toString());
+		assertEquals("word", s7.toString());
+		assertEquals("Hello World! My name is Tony.Why hello Mark!", s8.toString());
+		assertEquals("Hello Why hello Mark!World! My name is Tony.", s9.toString());
 	}
 	
 	/**
@@ -97,14 +127,18 @@ public class SentenceTest {
 	}
 	
 	/**
-	 * tests remove method
+	 * tests remove methods
 	 */
 	@Test
 	public void removeTest() {
 		s2.remove("Hello");
 		s3.remove(s3.getSize()-1);
+		s5.remove(s5.getSize());
+		s6.remove(".");
 		assertEquals(" ", s2.toString());
 		assertEquals("Hello World! My name is Tony", s3.toString());
+		assertEquals("Hello ", s5.toString());
+		assertEquals("Hello ", s6.toString());
 	}
 
 	/**
@@ -125,5 +159,47 @@ public class SentenceTest {
 		assertEquals(0, s1.getNumberOfWords());
 		assertEquals(1, s2.getNumberOfWords());
 		assertEquals(6, s3.getNumberOfWords());
+	}
+	
+	/**
+	 * Testing the exceptions in the program
+	 */
+	@Test // (expected = IllegalArgumentException.class)
+	public void exceptiontesting() {
+		try {
+			s1.get(1);
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid index", e.getMessage());
+		}
+		
+		try {
+			s1.getWord(5);
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid index", e.getMessage());
+		}
+		
+		try {
+			s1.getHead().get(1);
+		} catch (IllegalArgumentException e) {
+			assertEquals("wrong index", e.getMessage());
+		}
+		
+		try {
+			s1.getHead().getWord(1);
+		} catch (IllegalArgumentException e) {
+			assertEquals("wrong index", e.getMessage());
+		}
+		
+		try {
+			s1.add(1, "word");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid index to add an element", e.getMessage());
+		}
+		
+		try {
+			s1.getHead().add(1, "word");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid index to add an element", e.getMessage());
+		}
 	}
 }
